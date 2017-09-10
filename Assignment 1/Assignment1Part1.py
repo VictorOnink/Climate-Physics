373deccd55055f682c91e61df2a3c0949d95f575
VictorOnink <31734765+VictorOnink@users.noreply.github.com>
2017-09-10 16:56:01 +0200

    Final Code Assignment 1 MAIO

--- Assignment 1/Assignment1Part1.py
+++ Assignment 1/Assignment1Part1.py
@@ -9,10 +9,12 @@ First MAIO assignment for the analysis of AR and MA processes
 
 import numpy as np
 import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.graphics.tsaplots import plot_pacf
 
 # Define the constants of the AR(2) process
-alpha_1=[0]
-alpha_2=[0.75]
+alpha_1=[-0.40]
+alpha_2=[0.6]
 points=100
 
 #Function for determining the autocorrelation for a given lag tau
@@ -30,7 +32,7 @@ data=np.ones((points,len(alpha_1)))
 #set vector for the autocovariance
 autocor=np.zeros((len(data[:,0])-1,len(alpha_1)))
 #white noise generator
-noise_gen=1 #if zero, then noise will not be generated, if 1, then it will be
+noise_gen=0 #if zero, then noise will not be generated, if 1, then it will be
 mean_wn=0
 std_wn=1
 if noise_gen==0:
@@ -51,12 +53,27 @@ for i in range(len(alpha_1)):
     
     for tau in range(len(autocor[:,i])):
         autocor[tau,i]=autocorrelation(tau,data[:,i])
-
+    
 #time for plotting
 plt.figure(1)
 plt.plot(data)
-plt.title('AR(2) Function Output')
+plt.title('AR(2) Function Output, $a_1=-0.4, a_2=0.6$')
+plt.savefig('function0_4_0_6.pdf')
 
 plt.figure(2)
-plt.plot(autocor)
-plt.title('AR(2) Autocorrelation')
+acor_data=plot_acf(data)
+plt.title('Autocorrelation AR(2), $a_1=-0.4, a_2=0.6$')
+plt.xlabel('Lag')
+plt.ylabel('Correlation')
+plt.xlim([-0.5,10])
+plt.savefig('autocor0_4_0_6')
+plt.show(acor_data)
+
+plt.figure(3)
+pacor_data=plot_pacf(data, lags=100)
+plt.title('Partial Autocorrelation AR(2), $a_1=-0.4, a_2=0.6$')
+plt.xlabel('Lag')
+plt.ylabel('Correlation')
+plt.xlim([-0.5,10])
+plt.savefig('parautocor0_4_0_6')
+plt.show(pacor_data)
--- Assignment 1/Assignment1Part2.py
+++ Assignment 1/Assignment1Part2.py
@@ -10,6 +10,13 @@ Analysis and coding coefficient determination data sets
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.graphics.tsaplots import plot_pacf
+
+import statsmodels.formula.api as smf
+import statsmodels.tsa.api as smt
+import statsmodels.api as sm
+import scipy.stats as scs
 
 # import all the datasets
 data_a=pd.read_fwf('data_a.txt')
@@ -18,4 +25,52 @@ data_c=pd.read_fwf('data_c.txt')
 data_d=pd.read_fwf('data_d.txt')
 data_e=pd.read_fwf('data_e.txt')
 
-plt.plot(data_e)
+#determine which of the data sets you want to analyse at each point
+# 1= a, 2= b, 3= c, 4= d, 5= e
+# also, we detrend all the data to get rid of linear trends
+dataset=5
+if dataset==1:
+    data=data_a.values
+    data=statsmodels.tsa.tsatools.detrend(data, order=1,axis=0)
+elif dataset==2:
+    data=data_b.values
+    data=statsmodels.tsa.tsatools.detrend(data, order=1,axis=0)
+elif dataset==3:
+    data=data_c.values
+    data=statsmodels.tsa.tsatools.detrend(data, order=1,axis=0)
+elif dataset==4:
+    data=data_d.values
+    data=statsmodels.tsa.tsatools.detrend(data, order=1,axis=0)
+elif dataset==5:
+    data=data_e.values
+    data=statsmodels.tsa.tsatools.detrend(data, order=1,axis=0)
+    
+#Plot the autocorrelation functions for the selected dataset
+plt.figure(1)
+plt.plot(data)
+plt.title('data_e')
+plt.savefig('data_e.pdf')
+
+plt.figure(2)
+acor_data=plot_acf(data)
+plt.title('Autocorrelation Function for data_e')
+plt.xlabel('Lag')
+plt.ylabel('Correlation')
+plt.xlim([-0.5,10])
+plt.savefig('data_e_autocor.pdf')
+plt.show(acor_data)
+
+plt.figure(3)
+pacor_data=plot_pacf(data, lags=100)
+plt.title('Partial Autocorrelation Function for data_e')
+plt.xlabel('Lag')
+plt.ylabel('Correlation')
+plt.xlim([-0.5,20])
+plt.savefig('data_e_partialcor.pdf')
+plt.show(pacor_data)
+
+#Best fit section, determine the coefficient values
+max_lag = 30
+mdl = smt.ARMA(data, order=(2,0)).fit(
+        maxlag=max_lag, method='css', trend='nc')
+print mdl.summary() 
